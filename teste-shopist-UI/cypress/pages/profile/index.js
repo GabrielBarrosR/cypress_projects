f1f8e7dffbe26a8974c7764f9f62f633b66e8e70
GabrielBarrosR <bielbarrosrodrigues@gmail.com>
2025-09-29 12:35:39 -0300

    Realizado mais alguns testes do formulário da página de login e ajustado função do Form para aceitar valores nulos

--- teste-shopist-UI/cypress/pages/profile/index.js
+++ teste-shopist-UI/cypress/pages/profile/index.js
@@ -1,15 +1,31 @@
-class form {
-    fillForm(Firstname, Lastname, Address, Address2, city, state, Zipcode, phone_number){
-        cy.get('#firstname').clear().type(Firstname)
-        cy.get('#lastname').clear().type(Lastname)
-        cy.get('#address1').clear().type(Address)
-        cy.get('#address2').clear().type(Address2)
-        cy.get('#addressCity').clear().type(city)
-        cy.get('.multiselect__select').click();
-        cy.get('.multiselect__content').contains(state).scrollIntoView().click();                
-        cy.get('#addressZipcode').clear().type(Zipcode)
-        cy.get('#phone').clear().type(phone_number)
+class Form {
+  fillForm(Firstname, Lastname, Address, Address2, city, state, Zipcode, phone_number) {
+    cy.get('#firstname').clear()
+    if (Firstname) cy.get('#firstname').type(Firstname)
+
+    cy.get('#lastname').clear()
+    if (Lastname) cy.get('#lastname').type(Lastname)
+
+    cy.get('#address1').clear()
+    if (Address) cy.get('#address1').type(Address)
+
+    cy.get('#address2').clear()
+    if (Address2) cy.get('#address2').type(Address2)
+
+    cy.get('#addressCity').clear()
+    if (city) cy.get('#addressCity').type(city)
+
+    if (state) {
+      cy.get('.multiselect__select').click()
+      cy.get('.multiselect__content').contains(state).scrollIntoView().click()
     }
+
+    cy.get('#addressZipcode').clear()
+    if (Zipcode) cy.get('#addressZipcode').type(Zipcode)
+
+    cy.get('#phone').clear()
+    if (phone_number) cy.get('#phone').type(phone_number)
+  }
 }
 
-export default new form();
+export default new Form()
